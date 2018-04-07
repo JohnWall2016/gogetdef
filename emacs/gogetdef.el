78a60bf059bd159cf9a5f34cfce4f320b75667b0
John Wall <5672618@qq.com>
2018-04-07 10:15:01 +0800

    remove go-mode.el and there is gogetdef.el

---
+++ emacs/gogetdef.el
@@ -0,0 +1,89 @@
+(require 'go-mode)
+
+(defun gogetdef (point)
+  "Use the gogetdef tool to find the definition for an identifier at POINT.
+
+You can install gogetdef with 'go get -u github.com/JohnWall2016/gogetdef'."
+  (if (not (buffer-file-name (go--coverage-origin-buffer)))
+      ;; TODO: gogetdef supports unsaved files, but not introducing
+      ;; new artifical files, so this limitation will stay for now.
+      (error "Cannot use gogetdef on a buffer without a file name"))
+  (let ((buff (go--coverage-origin-buffer))
+        (posn (if (eq system-type 'windows-nt)
+                  (format "%s:#%d" (file-truename buffer-file-name) (1- (position-bytes point)))
+                (format "%s:#%d" (shell-quote-argument (file-truename buffer-file-name)) (1- (position-bytes point)))))
+        (out (godoc--get-buffer "<at point>")))
+  (with-current-buffer (get-buffer-create "*go-getdef-input*")
+    (setq-local inhibit-eol-conversion t)
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (go--insert-modified-files buff)
+    (call-process-region (point-min) (point-max) "gogetdef" nil out nil
+                         "-modified"
+                         (format "-pos=%s" posn)))
+  (prog1
+      (with-current-buffer out
+        (split-string (buffer-substring-no-properties (point-min) (point-max)) "\n" nil))
+    (kill-buffer out))))
+
+(defun gogetdef-jump (point &optional other-window)
+  "Jump to the definition of the expression at POINT."
+  (interactive "d")
+  (condition-case nil
+      (let* ((iod (gogetdef point))
+             (sig (nth 0 iod))
+             (pos (nth 1 iod)))
+        (if (not (string= "gogetdef-return" sig))
+            (message "%s" (mapconcat #'identity iod "\n"))
+          (if (or (not pos) (string= pos ""))
+              (message "No definition found for expression at point %S" iod)
+            (push-mark)
+            (if (eval-when-compile (fboundp 'xref-push-marker-stack))
+                ;; TODO: Integrate this facility with XRef.
+                (xref-push-marker-stack)
+              (ring-insert find-tag-marker-ring (point-marker)))
+            (godef--find-file-line-column pos other-window))))
+    (file-error (message "Could not run gogetdef binary"))))
+
+(defun gogetdef-describe (point)
+  "Describe the expression at POINT."
+  (interactive "d")
+  (condition-case nil
+      (let* ((iod (gogetdef point))
+             (sig (car iod))
+             (dcl (cddr iod)))
+        (if (not (string= "gogetdef-return" sig))
+            (message "%s" (mapconcat #'identity iod "\n"))
+          (if (not dcl)
+              (message "No definition found for expression at point %S" iod)
+            (message "%s" (mapconcat #'identity dcl "\n")))))
+    (file-error (message "Could not run gogetdef binary"))))
+
+(defun gogetdef-all (point)
+  "Use the gogetdef tool to find the definition for an identifier at POINT.
+
+You can install gogetdef with 'go get -u github.com/JohnWall2016/gogetdef'."
+  (if (not (buffer-file-name (go--coverage-origin-buffer)))
+      ;; TODO: gogetdef supports unsaved files, but not introducing
+      ;; new artifical files, so this limitation will stay for now.
+      (error "Cannot use gogetdef on a buffer without a file name"))
+  (let ((buff (go--coverage-origin-buffer))
+        (posn (if (eq system-type 'windows-nt)
+                  (format "%s:#%d" (file-truename buffer-file-name) (1- (position-bytes point)))
+                (format "%s:#%d" (shell-quote-argument (file-truename buffer-file-name)) (1- (position-bytes point)))))
+        (out (godoc--get-buffer "<at point>")))
+  (with-current-buffer (get-buffer-create "*go-getdef-input*")
+    (setq-local inhibit-eol-conversion t)
+    (setq buffer-read-only nil)
+    (erase-buffer)
+    (go--insert-modified-files buff)
+    (call-process-region (point-min) (point-max) "gogetdef" nil out nil
+                         "-modified"
+                         "-all"
+                         (format "-pos=%s" posn)))
+  (with-current-buffer out
+    (goto-char (point-min))
+    (godoc-mode)
+    (display-buffer (current-buffer) t))))
+
+(provide 'gogetdef)
